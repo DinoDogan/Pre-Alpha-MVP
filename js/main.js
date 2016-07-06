28446cb94120a1aec8552d60183bd8ab36e22e04
Dino Dogan <dino.dogan@gmail.com>
2016-07-06 13:07:51 -0400

    Added Cloud Carousel
    Update styles and JS, added jquery.easing.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,84 @@
-var $header = $("#header");
-var $home = $("#home");
+/**
+ * Header scroll stuff
+ */
+(function () {
 
-$(window).scroll(function(){
-    var scrollTop = document.body.scrollTop;
+    var $header = $("#header");
+    var $home = $("#home");
+    var $home_height = null;
 
-    if (scrollTop > ($home.height() - 200)) {
-        $header.addClass("header--blue");
+    $(window).scroll(function () {
+        var scrollTop = document.body.scrollTop;
+
+        if (scrollTop > ($home_height - 200)) {
+            $header.addClass("header--blue");
+        }
+        else {
+            $header.removeClass("header--blue");
+        }
+    });
+
+    $(window).resize(function () {
+        $home_height = $home.height();
+    });
+
+    // on page load do this:
+    $(window).trigger("resize");
+}());
+
+/**
+ * Hero cloud carousel
+ */
+(function () {
+    var $carouselItemsParent = $(".cloud-carousel-dest");
+    var $carouselItems = $carouselItemsParent.children();
+
+    var $carouselBtnsParent = $(".cloud-carousel-btns");
+    var $carouselBtns = [];
+
+    var currentPosition = 0;
+    var positionMax = $carouselItems.length - 1;
+    var timeout = "0";
+
+    function goTo(newPosition) {
+
+        // clear active state of old button (corresponding to carousel item)
+        $carouselBtns.eq(currentPosition).removeClass("is-active");
+
+        var $carouselItem = $carouselItems.eq(newPosition);
+
+        // add active state to new button (corresponding to carousel item)
+        $carouselBtns.eq(newPosition).addClass("is-active");
+
+        $carouselItemsParent.animate({
+            scrollLeft: $carouselItem.get(0).offsetLeft
+        }, 1200, "easeInOutExpo");
+
+        currentPosition = newPosition;
+
+        timeout = setTimeout(function(){
+            var _position = currentPosition++;
+
+            if (_position > positionMax)
+                _position = 0;
+
+            goTo(_position);
+        }, 3000);
     }
-    else {
-        $header.removeClass("header--blue");
-    }
-});
+
+    $carouselItems.each(function(i){
+        var $newBtn = $("<span>");
+        $carouselBtnsParent.append($newBtn);
+
+        $newBtn.click(function(){
+            clearTimeout(timeout);
+            goTo(i);
+        });
+    });
+
+    // update now that carousel btns are populated
+    $carouselBtns = $carouselBtnsParent.children();
+
+    // init
+    goTo(0);
+}());
