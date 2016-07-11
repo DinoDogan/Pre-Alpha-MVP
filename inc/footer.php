9a401369448e881f772c84e49941c394e6c330c0
Dino <dino.dogan@gmail.com>
2016-07-11 15:38:09 -0400

    add header.php + footer.php

---
+++ inc/footer.php
@@ -0,0 +1,72 @@
+<!-- Footer-Home sections -->
+<section id="footer-home">
+    <div class="home-background"></div>
+    <div class="call-to-action content">
+        <div class="cloud-upload">
+            <div class="cloud-upload-src">
+                <span>PDF</span>
+
+                <!--<img src="img/cloud-icon-active.png">-->
+                <a href="index2.html"><img src="img/cloud-icon.png"></a>
+                <!--<i class="fa fa-cloud-upload" aria-hidden="true"></i>-->
+            </div>
+        </div>
+        <div class="cloud-arrow">
+            <img src="img/arrow-icon.png">
+            <!--<i class="fa fa-long-arrow-right" aria-hidden="true"></i>-->
+        </div>
+        <div class="cloud-carousel">
+            <div class="cloud-carousel-inner">
+                <img src="img/page-icon.png">
+
+                <div class="cloud-carousel-dest">
+                    <span>WordPress</span>
+                    <span>Facebook<small>coming soon</small></span>
+                    <span>Medium<small>coming soon</small></span>
+                </div>
+
+                <div class="cloud-carousel-btns">
+                    <!-- Populated by JavaScript -->
+                </div>
+                <!--<i class="fa fa-file-o" aria-hidden="true"></i>-->
+            </div>
+        </div>
+    </div>
+</section>
+<div class="content">
+    <div id="footer">
+        <div id="footer-links">
+            <a href="#company">Company</a>
+            <a href="#services">Services</a>
+            <a href="#blog">Blog</a>
+            <a href="#tos">TOS</a>
+        </div>
+        <div id="footer-social">
+            <a href="https://twitter.com/MagnificentMktg" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="https://www.facebook.com/pages/Magnificent-Marketing/848067331941157" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="https://www.pinterest.com/MagnificentMktg/" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="js/jquery.easing.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
---
+++ inc/header.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>PDF to WordPress</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+          crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"-->
+    <!--integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"-->
+    <!--crossorigin="anonymous">-->
+
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Latest compiled and minified bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional bootstrap theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!-- Add your site or application content here -->
+
+<!-- navbar section -->
+<div id="header">
+    <div class="content">
+        <div id="header-left">
+            <a href="#home">
+                <div id="logo">Magnificent</div>
+            </a>
+        </div>
+        <div id="header-right">
+            <div id="header-links">
+                <a href="#about">About</a>
+                <a href="#faq">FAQ</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </div>
+    </div>
+</div>
