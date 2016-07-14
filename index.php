3c296d2e33024b1e63a2559478229c70454414d8
Dino <dino.dogan@gmail.com>
2016-07-14 11:17:32 -0400

    testing .htaccess

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <i class="modal-header-hint fa fa-question-circle-o"
-                       data-help="<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>"></i>
+                       data-help="<p>containing Lorem Ipsum passages, and more Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>"></i>
                     <h4 class="modal-title">Upload to Magnificent</h4>
                 </div>
                 <div class="modal-body">
