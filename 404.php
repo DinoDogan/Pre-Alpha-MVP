36da5df801e23334b4e9ae08a7f0adb347ffee7a
Dino <dino.dogan@gmail.com>
2016-07-14 12:45:14 -0400

    add 404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+    $title="404 | page not found";
+    include "inc/header.php";
+?>
+
+<section> 
+    <div class="content text">
+        <h1>404 | page not found</h1>
+        <a href="/pdf2wp/">&lsaquo; go home</a>
+    </div>
+</section>
+
+<?php include "inc/footer.php"; ?>
