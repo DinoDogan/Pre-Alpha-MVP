a717b07a5df69f01888b7afa5e4dae1ccfc5b457
Dino Dogan <dino.dogan@gmail.com>
2016-07-21 15:15:35 -0400

    Update faq.php

--- faq.php
+++ faq.php
@@ -8,7 +8,7 @@ include "inc/header.php";
         <p class="faq-question">What is Magnificent?</p>
         <p class="faq-answer">Magnificent is an Austin, TX based marketing agency servicing the publishing industry.</p>
         <p class="faq-question">Why would I want to convert PDFs to WordPress?</p>
-        <p class="faq-answer">Many of our clients have piles of PDFed content -such as magazines, instructional texts, and historically-significant documents- that they would like to publish to their blog for mobile-friendly, accessible-from-anywhere, easy-access by all. That's where we come in. </p>
+        <p class="faq-answer">Many of our clients have piles of PDFed content (such as magazines, instructional texts, and historically-significant documents) that they publish to their blog for mobile-friendly, accessible-from-anywhere, easy-access by all. That's where we come in. </p>
         <p class="faq-question">Where do animals go when their tails fall off?</p>
         <p class="faq-answer">The retail store.</p>
         <p class="faq-question">What do you call a fake noodle?</p>
